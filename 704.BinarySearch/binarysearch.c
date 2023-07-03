33a5a7eaf81462895e3fc67c02c0bd9b84136a68
Jaughn12 <ebukaawuzie@gmail.com>
2023-07-03 17:35:39 +0000

    Fixed C code for 704

--- 704.BinarySearch/binarysearch.c
+++ 704.BinarySearch/binarysearch.c
@@ -1,30 +1,27 @@
-#include <stdio.h>
 
-int binarySearch(int nums[], int target, int start, int end) {
-    if (start <= end) {
-        int mid = start + (end - start) / 2;
+int binarySearch(int* nums, int start, int end, int target) {
+    // Base case: If the search range is invalid, return -1
+    if (start > end)
+        return -1;
 
-        if (nums[mid] == target) {
-            return mid;  // Found the target element
-        } else if (nums[mid] < target) {
-            // Target is in the right half of the remaining range
-            return binarySearch(nums, target, mid + 1, end);
-        } else {
-            // Target is in the left half of the remaining range
-            return binarySearch(nums, target, start, mid - 1);
-        }
-    }
+    // Calculate the middle index
+    int mid = start + (end - start) / 2;
 
-    return -1;  // Target element is not found
+    // If the middle element is the target, return its index
+    if (nums[mid] == target)
+        return mid;
+
+    // If the middle element is less than the target, search the right half
+    if (nums[mid] < target)
+        return binarySearch(nums, mid + 1, end, target);
+
+    // If the middle element is greater than the target, search the left half
+    if (nums[mid] > target)
+        return binarySearch(nums, start, mid - 1, target);
 }
 
-int main() {
-    int nums[] = {-1, 0, 3, 5, 9, 12};
-    int target = 9;
-    int size = sizeof(nums) / sizeof(nums[0]);
-    int result = binarySearch(nums, target, 0, size - 1);
-
-    printf("%d\n", result);  // Print the index of the target element
-
-    return 0;
+int search(int* nums, int numsSize, int target) {
+    // Call the binarySearch helper function with initial parameters
+    return binarySearch(nums, 0, numsSize - 1, target);
 }
+
